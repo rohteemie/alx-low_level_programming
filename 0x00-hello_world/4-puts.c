e2ae92dc8200eb140ea200e913731887c16caf42
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2022-04-07 10:20:57 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - entry oint
+ *
+ * Return: 0 after printing the function
+ */
+int main(void)
+{
+	puts("\"Programming is  like building a multilingual puzzle");
+	return (0);
+}
