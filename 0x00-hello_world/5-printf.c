3064f7e4ecd38eb3622ee5072efb892d9136dca6
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2022-04-07 11:15:19 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * The MAIN entry point
+ *
+ * use of function printf
+ *
+ * output should be zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
