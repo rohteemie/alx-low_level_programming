de9e61fdf47bde711d5cae1ba349bdf24b0c9aae
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2022-04-10 02:58:23 -0700

    Task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) Successfull
+ */
+int main(void)
+{
+	char i = 'a';
+
+	for (i = 'a'; i <= 'z'; ++i)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
