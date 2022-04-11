a6866633fff767eef01dcb5df92bfa1e9a61d54b
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2022-04-10 18:23:19 -0700

    Task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) Successfull
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x != 'e' && x != 'q')
+		{
+			putchar(x);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
