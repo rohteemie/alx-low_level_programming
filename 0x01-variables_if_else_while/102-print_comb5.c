deca0a872ede69669b83da098c34faad47b2682f
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2022-04-11 05:50:19 -0700

    Task102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - print combination of digits
+ *
+ * Return: return (0)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 98; a++)
+	{
+		for (b = a + 1; b <= 99; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(32);
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+
+			if (a / 10 != 9 || a % 10 != 8)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
