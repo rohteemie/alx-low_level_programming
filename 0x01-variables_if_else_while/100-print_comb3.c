041bf6d87996c67f53c47bcd9d6b4e338d7be23e
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2022-04-11 05:14:44 -0700

    Task100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point to print all combination
+ * of two digit with comma, and space followed by new line
+ *
+ * Return: Always (0) successfull
+ */
+int main(void)
+{
+	int numb1, numb2;
+
+		for (numb1 = 0; numb1 < 9; numb1++)
+		{
+			for (numb2 = numb1 + 1; numb2 < 10; numb2++)
+			{
+				putchar((numb1 % 10) + '0');
+				putchar((numb2 % 10) + '0');
+
+				if (numb1 == 8 && numb2 == 9)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	putchar('\n');
+	return (0);
+}
