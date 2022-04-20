9835c4bdaa5925585129ef10c8c07ee5335b107a
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2022-04-20 14:29:44 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,11 +11,13 @@ void puts_half(char *str)
 
 	for (len = 0; str[len] != '\0'; len++)
 	{
-		if ((len % 2) == 0)
-			m = (len / 2);
-		else
-			m = (len + 1) / 2;
 	}
+	
+	if ((len % 2) == 0)
+		m = (len / 2);
+	else
+		m = (len + 1) / 2;
+
 	for (i = m; i < len; i++)
 	{
 		_putchar(str[i]);
