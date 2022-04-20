1cb4c1f2f712663b76c52ece150f341652fdfdb5
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2022-04-20 08:09:42 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/**
+ * swao_int - switches value of first input withvthe value of the second input
+ * @a: First value to swap
+ * @b: Second value to swap
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
