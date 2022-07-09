701c6a3e58e23662f0c651eeaf78b519e99bb82c
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2022-07-09 13:29:21 +0000

    Print element of a doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,21 @@
 #include "lists.h"
+#include <stdlib.h>
+
+/**
+ * print_dlistint - prints the elements of a dlistint_t
+ * @h: function param/variable of a user define dataType
+ * Return: numbers of node
+ */
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	//code to Return the number of nodes here
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
